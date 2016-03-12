9911ada2af01e298af08a3bb34dd4b80a4dded28
Arnd Brugman <arndbrugman@gmail.com>
2016-03-12 11:11:00 +0100

    Added Team Badges and Points Calculation
    Leaderboard is sorted by Points

--- js/Leaderboard.js
+++ js/Leaderboard.js
@@ -10,11 +10,24 @@ angular.module('Leaderboard').controller('LeaderboardController', function($scop
   $http.get('Teams.json').
     success(function(data, status, headers, config) {
       $scope.Teams = data.Teams;
+      for (TeamNr in $scope.Teams) {
+        console.log($scope.Teams[TeamNr]);
+        $scope.Teams[TeamNr].Points = 0;
+        for (Badge in $scope.Teams[TeamNr].Badges) {
+          console.log(Badge, $scope.Teams[TeamNr].Badges[Badge]);
+          if ($scope.Teams[TeamNr].Badges[Badge] === "Bronze"   ) $scope.Teams[TeamNr].Points += 1;
+          if ($scope.Teams[TeamNr].Badges[Badge] === "Silver"   ) $scope.Teams[TeamNr].Points += 2;
+          if ($scope.Teams[TeamNr].Badges[Badge] === "Gold"     ) $scope.Teams[TeamNr].Points += 4;
+          if ($scope.Teams[TeamNr].Badges[Badge] === "Platinum" ) $scope.Teams[TeamNr].Points += 6;
+          if ($scope.Teams[TeamNr].Badges[Badge] === "Diamond"  ) $scope.Teams[TeamNr].Points += 8;
+        }
+      }
+      window.Teams = $scope.Teams;
     }).
     error(function(data, status, headers, config) {
       $scope.Teams = [
 	  {
-	  	'name': 'Teams not loaded',
+	  	'name': 'Unable to load Teams.json',
 	  	'url:': 'www',
 	  }, ];
     });
